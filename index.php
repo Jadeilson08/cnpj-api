f2b3a9c37244cd834b866897f679a38b339567e2
jadeilson <jadeilson08@gmail.com>
2020-04-29 14:27:00 -0400

    Mensagens de erro e limpeza de codigo

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+    header('Content-type: application/json;  charset=utf-8');
+    $path = explode('/', $_GET['url']);
+    $cnpj = validateCNPJ($path[1]);
+        if($_SERVER['REQUEST_METHOD'] === 'GET'){            
+            $cnpj = generateJSON($cnpj);
+            echo validateRequest($cnpj);
+        }else{
+            echo "Requisição POST é inválida";    
+        }
+    
+    #valida se há caracteres especiais
+    function validateCNPJ($cnpj)
+    {
+        $cnpj = preg_replace("/[^0-9]/", "", $cnpj);
+        if(strlen($cnpj)!=14){
+            echo "CNPJ Inválido";
+            exit;
+        }else{
+            return (string) $cnpj;
+        }
+    }
+
+    #Acessar o site www.receitaws.com.br e cria o JSON
+    function generateJSON($cnpj)
+    {
+        # https://www.receitaws.com.br/v1/cnpj/04391715000173
+        $url = "https://www.receitaws.com.br/v1/cnpj/".$cnpj;
+        $ci = curl_init($url);
+        curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, false);
+        $cnpj = json_decode(curl_exec($ci));
+        $cnpj = json_encode($cnpj);
+        return $cnpj;
+    }
+
+    #Valida se foi realizado muitas requisições ou se a conexão está indisponível
+    function validateRequest($cnpj)
+    {
+        if($cnpj != null && $cnpj != 'null'){
+            return $cnpj;
+        }else{
+            return "Muitas requisições";
+        }
+    }
